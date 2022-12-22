50d7f8c1012054b3cc5e972d09a1758154deee9a
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-12-22 22:31:25 +0300

    added component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Blog from './components/Blog';
 import './App.css';
 
 function App() {
   return (
     <div >
-      Hello World
+      <Navbar />
+      <Home />
+      <About />
+      <Services />
+      <Blog />
     </div>
   );
 }
